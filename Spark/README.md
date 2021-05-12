d533f3b9afefb93adbd65186fdd9e523fa9b9c8e
IakMastro <cse242017102@uniwa.gr>
2021-05-12 19:37:38 +0300

    Final Commit

---
+++ Spark/README.md
@@ -0,0 +1,6 @@
+# Apache Spark
+
+![Apache Spark Logo](img/Apache_Spark_logo.png)
+
+Το Apache Spark είναι ένα εναλλακτικό API του Hadoop, το οποίο δουλεύει 100 φόρες πιο γρήγορα, μπορεί να υλοποιηθεί ευκολότερα σε διάφορες γλώσσες, όπως στη Java, στη Scala και στη Python.
+Παραπάνω πληροφορίες για το [Spark](Spark/Spark.adoc).
--- Spark/src/main/scala/WordCount/WordCount.scala
+++ Spark/src/main/scala/WordCount/WordCount.scala
@@ -2,21 +2,19 @@ package WordCount
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object WordCount
-{
-    def main(args: Array[String]): Unit =
-    {
-        val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
 
-	val sc = new SparkContext(conf)
+    val sc = new SparkContext(conf)
 
-        val input = sc.textFile("file://" + System.getProperty("user.dir") + "/data/simple_text.txt")
+    val input = sc.textFile("file://" + System.getProperty("user.dir") + "/data/simple_text.txt")
 
-        val wordcount = input.flatMap(text => text.split(" "))
-          .map(word => (word.toLowerCase, 1))
-          .reduceByKey(_+_)
-          .foreach(println)
+    val wordcount = input.flatMap(text => text.split(" "))
+      .map(word => (word.toLowerCase, 1))
+      .reduceByKey(_ + _)
+      .foreach(println)
 
-        sc.stop()
-    }
+    sc.stop()
+  }
 }
